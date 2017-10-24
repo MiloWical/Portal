cf4c95b6e2ebd3dbb6a416cb5fb7b161bf52c7c1
Milo Wical <Milo.Wical@compare.com>
2017-10-24 16:41:24 -0400

    Added routing to the maion page to the app module.

--- Portal/src/app/app.module.ts
+++ Portal/src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component'
+
+const routes:Routes = [
+  { path: '', component: AppComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
